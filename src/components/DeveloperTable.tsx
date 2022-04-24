9a2f1db967f6c9cefae55a488b5ab3248175dc34
Verkunder <verkunder614@gmail.com>
2022-04-24 19:34:56 +0300

    Появилась возможность добавить разработчиков!

--- src/components/DeveloperTable.tsx
+++ src/components/DeveloperTable.tsx
@@ -1,4 +1,4 @@
-import React, {FC, useEffect, useState} from 'react';
+import React, {FC, useState} from 'react';
 import {IDevelopers} from "../types/types";
 import DeveloperItem from "./DeveloperItem";
 import AddDevelopers from "./AddDevelopers";
@@ -11,15 +11,13 @@ const DeveloperTable: FC<DeveloperTableProps> = ({developers}) => {
     const [newDevelopers, setDevelopers] = useState<IDevelopers[]>([])
 
     const addDevelopers = (name: string, skill: string, department: string) => {
-        developers.push({name: name, skill: skill, department: department})
+        const newDevelopers = developers.push({name: name, skill: skill, department: department})
         setDevelopers(developers)
     }
     const deliteItem = (id: number) => {
-        const dev = developers.splice(id, 1)
-        setDevelopers(dev)
+        const newDevelopers = developers.splice(id, 1)
+        setDevelopers(newDevelopers)
     }
-
-
 
     const tableViev = () => {
         if (!developers.length) {
@@ -36,7 +34,7 @@ const DeveloperTable: FC<DeveloperTableProps> = ({developers}) => {
                     <thead>
                     <tr>
                         <th scope="col">#</th>
-                        <th scope="col" >ФИО</th>
+                        <th scope="col">ФИО</th>
                         <th scope="col">Уровень навыков</th>
                         <th scope="col">Направление</th>
                         <th scope="col"> </th>
